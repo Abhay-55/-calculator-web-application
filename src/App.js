338c2033db8123f69a99c6efbc5896cd259f096e
Abhay Maity <144135835+Abhay-55@users.noreply.github.com>
2025-03-24 14:28:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './components/Header/Header';
-// Importing various functional components
+
 import Wrapper from './components/Wrapper/Wrapper';
 import Screen from './components/Screen/Screen';
 import ButtonBox from './components/ButtonBox/ButtonBox';
@@ -8,7 +8,6 @@ import Button from './components/Button/Button';
 import CalcProvider from './context/CalcContext';
 import Footer from './components/Footer/Footer';
 
-// creating btnValues Array
 const btnValues = [
   ["AC", "+/-", "%", "/"],
   [7, 8, 9, "x"],
@@ -17,7 +16,7 @@ const btnValues = [
   [0, ".", "="]
 ];
 
-// Functional Component App
+
 function App() {
   return (
     <div>
